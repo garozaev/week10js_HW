d7c73ae537a402943108f8ad4c11af59db784dec
garozaev <g.a.rozaev@gmail.com>
2023-11-12 18:06:38 +0300

    week10_practice1

--- week10.js
+++ week10.js
@@ -83,7 +83,7 @@ console.log(secondFunction(2, 3)); //Выведет 5
 let f ;
 let g ;
 
-function thirdFunction(f = 3, g = 2) {
+function thirdFunction(f = 3, g = 3) {
 	//let z = f;
 	//let p = g;
 	return f * g;
@@ -98,52 +98,117 @@ console.log(thirdFunction(f, g)); //Выведет 30
 //Задание 9
 // Напишите функцию приветствия (название придумайте самостоятельно), которая принимает один параметр guestName и возвращает приветствие в формате "Привет, {guestName}!". Если параметр не передан, используйте значение по умолчанию: "Гость". Чтобы увидеть результат в консоли, вызовите функцию внутри console.log.
 
+let guestName ;
+
+let greetMessage = (guestName = 'Гость') => 'Привет' + ', ' + guestName + '!';
+
+console.log (greetMessage(guestName));
+
 //Задание 10
 // Создайте функцию calculateDifference с помощью Function Expression и правильно используйте её, чтобы вычесть два числа и вывести результат в консоль.
 // Ваш код
 
-//const result = calculateDifference(5, 3);
+const calculateDifference = function(a, b) {
 
-//console.log(result); //Выведет 2
+    return a - b;
+
+};
+const result = calculateDifference(5, 3);
+
+console.log(result); //Выведет 2
 
 //Задание 11
 // Напишите функцию calculateProduct с помощью Function Expression, которая будет принимать два параметра. Функция должна перемножить значения и вывести результат в консоль.
 
-//const total = calculateProduct(5, 3);
+let calculateProduct = function(f, g) {
 
-//console.log(total); //Выведет 15
+	return f * g;
+}
+const total = calculateProduct(5, 3);
+
+console.log(total); //Выведет 15
 
 //Задание 12
 // Напишите стрелочную функцию calculateSquare, которая будет принимать один параметр num. Внутри функции объявите локальную переменную result и присвойте ей значение, равное квадрату числа num. Затем верните значение переменной result из функции.
 
-//const squaredNumber = calculateSquare(5);
+const calculateSquare = (num) => {
+	let result = (num * num);
+	return result;
+};
 
-//console.log(squaredNumber); // Выведет 25
+const squaredNumber = calculateSquare(5);
+
+console.log(squaredNumber); // Выведет 25
 
 //Задание 13
 // Напишите  стрелочную функцию sayThree, которая будет выводить в консоль сообщение "Три!"
 
+const sayTree = () => "Три";
+
+console.log(sayTree());
+
 //Задание 14
 // Напишите функцию findAnimal, которая будет выводить в консоль значения переменных cat и rabbit. Переменная cat объявлена внутри функции, а переменная rabbit объявлена во внешней области видимости.
 
+let rabbit = ('Пушок')
+const findAnimal = () => {
+	let cat = ('Мурзик');
+	return cat + ' и ' + rabbit;
+}
+
+console.log(findAnimal());
+
 //Задание 15
 // Допишите программу, которая вычисляет сколько лет прошло с летней олимпиады в Токио, Япония. Подсказка: 1. нужно создать переменную с текущим годом. 2. при вызове функции в круглые скобки нужно передать два параметра: текущий год и год летней олимпиады в Токио.
+
 const lastOlympicsYear = 2021;
 
-function calculateYearsSinceLastOlympics(a, b) {}
+let thisYear = 2023;
 
+function calculateYearsSinceLastOlympics(a, b) {
+    return a - b;
+}
 console.log(
-	'С момента летней олимпиады в Токио прошло ' + calculateYearsSinceLastOlympics() + ' года',
-);
+	'С момента летней олимпиады в Токио прошло ' + calculateYearsSinceLastOlympics(thisYear,lastOlympicsYear) + ' года.');
 
 //Задание 16
 // Напишите программу, которая вычисляет возраст пользователя на основе его года рождения, будет выводить результат в консоль.
+
 const friendYearOfBirth = 1985;
 
-function calculateAge() {}
+let todayYear = 2023;
+
+function calculateAge(c, d) {
+	return c - d;
+
+}
+
+console.log('Прияиель, тебе уже ' + calculateAge(todayYear, friendYearOfBirth) + ' лет.');
 
 //Задание 17
 // Напишите функцию которая принимает ваш год рождения и возвращает ваш возраст. Обратите ваше внимание, что в глобальной зоне видимости есть переменная с текущим годом.
 
+const yourYearBirth = 1;
+
+let yearNow = 2023;
+
+let calculateAgeNow = (x, y) => {
+	let age = (x - y);
+	return age;
+}
+
+console.log('Вам сейчас ' + calculateAgeNow(yearNow, yourYearBirth) + ' лет.');
+
+
 //Задание 18
 // Напишите функцию, которая должна возводить число в заданную степень. Результат должен выводить в консоль в формате: console.log("Результат: " + result);
+
+let num = 7;
+let degree = 2;
+
+const degreeNum = (a, n) => {
+	return a ** n;
+}
+
+let resultPower = degreeNum(num, degree);
+console.log('Результат ' + resultPower);
